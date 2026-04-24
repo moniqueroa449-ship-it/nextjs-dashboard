17cead2e9f0d125d7a333dee35ae0989ff0b8a9f
Monique <moniqueroa449@gmail.com>
2026-04-24 21:33:15 +0800

    updated and tested features

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <p>Dashboard Page</p>;
+  return <p className="text-indigo-300">Hello world</p>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,23 +8,26 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-indigo-300 p-4 md:h-52">
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-indigo-100 px-6 py-10 md:w-2/5 md:px-20">
+        <div className="relative w-10 h-10 rotate-[-45deg] bg-indigo-500">
+        <div className="absolute w-10 h-10 bg-indigo-500 rounded-full top-[-20px] left-0"></div>
+        <div className="absolute w-10 h-10 bg-indigo-500 rounded-full left-[20px] top-0"></div>
+        </div>
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}></p>
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <a href="https://nextjs.org/learn/" className="text-indigo-400">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-indigo-300 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-indigo-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
